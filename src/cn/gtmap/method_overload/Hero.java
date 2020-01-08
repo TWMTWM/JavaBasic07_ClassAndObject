fe309a86db83947922edecf8f0edc2a5c11da037
TWM <15152806231@163.com>
2020-01-08 08:31:26 +0800

    英雄类

---
+++ src/cn/gtmap/method_overload/Hero.java
@@ -0,0 +1,15 @@
+package cn.gtmap.method_overload;
+/**
+ * 英雄类
+ * @author Administrator
+ *
+ */
+public class Hero {
+    String name; //姓名
+    
+    float hp; //血量
+      
+    float armor; //护甲
+      
+    int moveSpeed; //移动速度
+}
